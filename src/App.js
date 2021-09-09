5110729e86d3670c4223b382faa49f2b9b7da059
고가영 <4723515@naver.com>
2021-09-09 23:55:36 +0900

    3장 리액트기초개념알아보기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+// import Potato from "./Potato";
 
-function App() {
-  return <div>Hello!!!!</div>
+const Food = (props) => {
+    // console.log(props);
+    let {fav} = props;
+    return (
+        <h3>i love {fav}</h3>
+    )
+
+}
+
+const App =()=> {
+    return (
+        <div>
+          <h3>Hello!!!!</h3>
+          <Food fav={"kimchi"}   /> {/*props는 문자열을 제외하고 전부 중괄호로감싸야함*/}
+          <Food fav={"ramen"}   />
+          <Food fav={"samgiopsal"}   />
+          <Food fav={"chukumi"}   />
+
+
+          {/*{index.html의 root와 이름이 같은 컴포넌트는 사용할 수 없다.}*/}
+        </div>
+    )
 
 }
 
---
+++ src/Potato.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Potato = () => {
+    return (
+        <h3>i love potato</h3>
+    )
+
+}
+export default Potato;
